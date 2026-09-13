ae5f894a73b4273a46c24f3ab2b0370aa403da99
longzdez-debug <longz.dez@gmail.com>
2026-09-13 07:58:21 +0300

    refactor: remove database migrations

--- drizzle/0002_shallow_wong.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "gender" varchar(32);--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "birthday" date;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "interests" text;
